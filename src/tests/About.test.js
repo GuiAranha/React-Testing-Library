97795003df1d9d68d928e7ffcff9dbe157869200
Guilherme Aranha <guilherme.aranha.ga@gmail.com>
2022-03-23 09:22:35 -0300

    Requisito 2 - ok

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,2 +1,27 @@
-test('', () => {});
-// inicio
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import About from '../components/About';
+import renderWithRouter from '../services/renderWithRouter';
+
+describe('Testes do componente About', () => {
+  test('Verifica se a pagina contem h2', () => {
+    renderWithRouter(<About />);
+    const heading = screen.getByRole('heading', { name: /about pokédex/i });
+    expect(heading).toBeInTheDocument();
+  });
+
+  test('Verifica se a pagina contem 2 paragrafos', () => {
+    renderWithRouter(<About />);
+    const paragraph1 = screen.getByText(/this application/i);
+    const paragraph2 = screen.getByText(/one can/i);
+    expect(paragraph1).toBeInTheDocument();
+    expect(paragraph2).toBeInTheDocument();
+  });
+
+  test('Verifica se a pagina contem imagem', () => {
+    renderWithRouter(<About />);
+    const image = screen.getByRole('img', { name: /pokédex/i });
+    expect(image.src).toContain('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
